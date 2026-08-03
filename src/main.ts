a0d0f3219258e9fdce27e9668b97d58c2e25a0b1
Paul Chidyausiku <paulchidyausiku@Pauls-Air.lan>
2026-08-03 19:08:59 +0100

    ORI-75/ORI-134 Configured swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'; 
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Oridox E-Commerce API')
+    .setDescription('API documentation for the Oridox E-Commerce backend.')
+    .setVersion('1.0')
+    .build(); 
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig); 
+
+  SwaggerModule.setup('api/docs', app, swaggerDocument); 
+
+  const port = process.env.PORT ?? 3000;
+
   await app.listen(process.env.PORT ?? 3000);
   Logger.log(`Server is running on http://localhost:${process.env.PORT ?? 3000}`);
 }
